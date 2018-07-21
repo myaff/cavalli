3caa1631bdef896e5f830e2394bdac6159ece43b
myaff <grabka74@gmail.com>
2018-07-22 03:26:03 +0500

    правки анимации, индикаторы, триггеры

--- src/js/components/animation.js
+++ src/js/components/animation.js
@@ -6,129 +6,173 @@
 function init () {
   let controller = new ScrollMagic.Controller();
   let tl = new TimelineMax()
-    .to(".screen--0 .anim-img", 1, 
-      {scale: 1.5, x: "-55%", y: "-70%", ease: Linear.easeNone})
+    // .screen--0 stage 1
+    .set(".screen--0 .anim-img", {className: "+=stage-1"}, "+=1")
+    .set(".screen--0 .dot--0", {className: "-=active"})
+    .set(".screen--0 .dot--1", {className: "+=active"})
     .to(".screen--0 .anim-title", 1, 
       {y: "-300%", ease: Linear.easeNone}, '-=1')
     .to(".header__text", 0.3, 
       {opacity: 0, ease: Linear.easeNone}, '-=1')
-    .to(".screen--0 .anim-text", 1, 
-      {y: "0%", ease: Linear.easeNone})
-    .to(".anim-bg.ll", 1, 
-      {opacity: 1, ease: Linear.ease}, '-=0.7')
-    .to(".screen--0 .anim-text", 1,
+    .to(".screen--0 .anim-text", 3, 
+      {y: "0%", ease: Linear.easeNone}, '+=2')
+    .to(".anim-bg.ll", 3, 
+      {opacity: 1, ease: Linear.ease}, '-=2')
+    .to(".screen--0 .anim-text", 3,
       {y: "-130%", ease: Linear.easeNone})
-    .to(".anim-bg.ll", 1, 
-      {opacity: 0, ease: Linear.ease}, '-=0.7')
-    .to(".screen--0", 1, 
+    .to(".anim-bg.ll", 3, 
+      {opacity: 0, ease: Linear.ease}, '-=2')
+    .set(".screen--0 .anim-img", {className: "+=stage-out"})
+    .set(".screen--0 .slide--0 .js-tp", {className: "+=active"})
+    .set(".screen--0 .slide--0 .js-tp", {className: "-=active"}, '+=2')
+    .to(".screen--0", 2, 
       {y: "-100%", ease: Linear.easeNone})
-    .to(".screen--0 .anim-img", 1, 
-      {scale: 1.1, ease: Linear.easeNone}, '-=1')
-    .to(".screen--1 .anim-img", 1, 
-      {scale: 2, x: "0%", y: "0%", ease: Linear.easeNone})
-    .to(".screen--1 .slide--0 .anim-text", 1, 
-      {y: "0%", ease: Linear.easeNone})
-    .to(".anim-bg.ll", 1, 
-      {opacity: 1, ease: Linear.ease}, '-=0.7')
-    .to(".screen--1 .slide--0 .anim-text", 1,
+    .to(".screen--0 .anim-img", 2, 
+      {scale: 1.1, y: "0%", ease: Linear.easeNone}, '-=2')
+    // .screen--1 stage 1
+    .set(".screen--1 .anim-img", {className: "+=stage-1"}, "+=1")
+    .set(".screen--1 .dot--0", {className: "-=active"})
+    .set(".screen--1 .dot--1", {className: "+=active"})
+    .to(".screen--1 .slide--0 .anim-text", 3, 
+      {y: "0%", ease: Linear.easeNone}, "+=2")
+    .to(".anim-bg.ll", 3, 
+      {opacity: 1, ease: Linear.ease}, '-=2')
+    .to(".screen--1 .slide--0 .anim-text", 3,
       {y: "-130%", ease: Linear.easeNone})
-    .to(".anim-bg.ll", 1, 
-      {opacity: 0, ease: Linear.ease}, '-=0.7')
-    .to(".screen--1 .anim-img", 1, 
-      {scale: 1.8, x: "-95%", y: "-15%", ease: Linear.easeNone})
-    .to(".screen--1 .slide--1 .anim-text", 1, 
-      {y: "0%", ease: Linear.easeNone})
-    .to(".anim-bg.br", 1, 
-      {opacity: 1, ease: Linear.ease}, '-=0.7')
-    .to(".screen--1 .slide--1 .anim-text", 1,
+    .to(".anim-bg.ll", 3, 
+      {opacity: 0, ease: Linear.ease}, '-=2')
+    .set(".screen--1 .slide--0 .js-tp", {className: "+=active"})
+    .set(".screen--1 .slide--0 .js-tp", {className: "-=active"}, '+=2')
+    // .screen--1 stage 2
+    .set(".screen--1 .anim-img", {className: "+=stage-2"}, "+=1")
+    .set(".screen--1 .dot--1", {className: "-=active"})
+    .set(".screen--1 .dot--2", {className: "+=active"})
+    .to(".screen--1 .slide--1 .anim-text", 3, 
+      {y: "0%", ease: Linear.easeNone}, "+=2")
+    .to(".anim-bg.br", 3, 
+      {opacity: 1, ease: Linear.ease}, '-=2')
+    .to(".screen--1 .slide--1 .anim-text", 3,
       {y: "-130%", ease: Linear.easeNone})
-    .to(".anim-bg.br", 1, 
-      {opacity: 0, ease: Linear.ease}, '-=0.7')
-    .to(".screen--1", 1, 
+    .to(".anim-bg.br", 3, 
+      {opacity: 0, ease: Linear.ease}, '-=2')
+    .set(".screen--1 .anim-img", {className: "+=stage-out"})
+    .set(".screen--1 .slide--1 .js-tp", {className: "+=active"})
+    .set(".screen--1 .slide--1 .js-tp", {className: "-=active"}, '+=2')
+    .to(".screen--1", 2, 
       {y: "-100%", ease: Linear.easeNone})
-    .to(".screen--1 .anim-img", 1, 
-      {y: "90%", ease: Linear.easeNone}, '-=1')
-    .to(".screen--2 .anim-img", 1, 
-      {scale: 1.5, x: "-25%", y: "-25%", ease: Linear.easeNone})
-    .to(".screen--2 .anim-text", 1, 
-      {y: "0%", ease: Linear.easeNone})
-    .to(".anim-bg.ll", 1, 
-      {opacity: 1, ease: Linear.ease}, '-=0.7')
-    .to(".screen--2 .anim-text", 1,
+    .to(".screen--1 .anim-img", 2, 
+      {y: "70%", ease: Linear.easeNone}, '-=2')
+    // .screen--2 stage 1
+    .set(".screen--2 .anim-img", {className: "+=stage-1"}, "+=1")
+    .set(".screen--2 .dot--0", {className: "-=active"})
+    .set(".screen--2 .dot--1", {className: "+=active"})
+    .to(".screen--2 .anim-text", 3, 
+      {y: "0%", ease: Linear.easeNone}, "+=2")
+    .to(".anim-bg.ll", 3, 
+      {opacity: 1, ease: Linear.ease}, '-=2')
+    .to(".screen--2 .anim-text", 3,
       {y: "-130%", ease: Linear.easeNone})
-    .to(".anim-bg.ll", 1, 
-      {opacity: 0, ease: Linear.ease}, '-=0.7')
-    .to(".screen--2", 1, 
+    .to(".anim-bg.ll", 3, 
+      {opacity: 0, ease: Linear.ease}, '-=2')
+    .set(".screen--2 .anim-img", {className: "+=stage-out"})
+    .set(".screen--2 .slide--0 .js-tp", {className: "+=active"})
+    .set(".screen--2 .slide--0 .js-tp", {className: "-=active"}, '+=2')
+    .to(".screen--2", 2, 
       {y: "-100%", ease: Linear.easeNone})
-    .to(".screen--2 .anim-img", 1, 
-      {scale: 1.1, x: "-50%", y: "-50%", ease: Linear.easeNone}, '-=1')
-    .to(".screen--3 .anim-img", 1, 
-      {scale: 1.9, x: "-2%", y: "-30%", ease: Linear.easeNone})
-    .to(".screen--3 .slide--0 .anim-text", 1, 
-      {y: "0%", ease: Linear.easeNone})
-    .to(".anim-bg.bl", 1, 
-      {opacity: 1, ease: Linear.ease}, '-=0.7')
-    .to(".screen--3 .slide--0 .anim-text", 1,
+    .to(".screen--2 .anim-img", 2, 
+      {scale: 1.1, x: "-50%", y: "-50%", ease: Linear.easeNone}, '-=2')
+    // .screen--3 stage 1
+    .set(".screen--3 .anim-img", {className: "+=stage-1"}, "+=1")
+    .set(".screen--3 .dot--0", {className: "-=active"})
+    .set(".screen--3 .dot--1", {className: "+=active"})
+    .to(".screen--3 .slide--0 .anim-text", 3, 
+      {y: "0%", ease: Linear.easeNone}, "+=2")
+    .to(".anim-bg.bl", 3, 
+      {opacity: 1, ease: Linear.ease}, '-=2')
+    .to(".screen--3 .slide--0 .anim-text", 3,
       {y: "-130%", ease: Linear.easeNone})
-    .to(".anim-bg.bl", 1, 
-      {opacity: 0, ease: Linear.ease}, '-=0.7')
-    .to(".screen--3 .anim-img", 1, 
-      {scale: 2.1, x: "-106%", y: "-5%", ease: Linear.easeNone})
-    .to(".screen--3 .slide--1 .anim-text", 1, 
-      {y: "0%", ease: Linear.easeNone})
-    .to(".anim-bg.lr", 1, 
-      {opacity: 1, ease: Linear.ease}, '-=0.7')
-    .to(".screen--3 .slide--1 .anim-text", 1,
+    .to(".anim-bg.bl", 3, 
+      {opacity: 0, ease: Linear.ease}, '-=2')
+    .set(".screen--3 .slide--0 .js-tp", {className: "+=active"})
+    .set(".screen--3 .slide--0 .js-tp", {className: "-=active"}, '+=2')
+    // .screen--3 stage 2
+    .set(".screen--3 .anim-img", {className: "+=stage-2"}, "+=1")
+    .set(".screen--3 .dot--1", {className: "-=active"})
+    .set(".screen--3 .dot--2", {className: "+=active"})
+    .to(".screen--3 .slide--1 .anim-text", 3, 
+      {y: "0%", ease: Linear.easeNone}, "+=2")
+    .to(".anim-bg.lr", 3, 
+      {opacity: 1, ease: Linear.ease}, '-=2')
+    .to(".screen--3 .slide--1 .anim-text", 3,
       {y: "-130%", ease: Linear.easeNone})
-    .to(".anim-bg.lr", 1, 
-      {opacity: 0, ease: Linear.ease}, '-=0.7')
-    .to(".screen--3", 1, 
+    .to(".anim-bg.lr", 3, 
+      {opacity: 0, ease: Linear.ease}, '-=2')
+    .set(".screen--3 .anim-img", {className: "+=stage-out"})
+    .set(".screen--3 .slide--1 .js-tp", {className: "+=active"})
+    .set(".screen--3 .slide--1 .js-tp", {className: "-=active"}, '+=2')
+    .to(".screen--3", 2, 
       {y: "-100%", ease: Linear.easeNone})
-    .to(".screen--3 .anim-img", 1, 
-      {scale: 2, y: "75%", ease: Linear.easeNone}, '-=1')
-    .to(".screen--4 .anim-img", 1, 
-      {scale: 1.8, x: "-50%", y: "-15%", ease: Linear.easeNone})
-    .to(".screen--4 .anim-text", 1, 
-      {y: "0%", ease: Linear.easeNone})
-    .to(".anim-bg.bl", 1, 
-      {opacity: 1, ease: Linear.ease}, '-=0.7')
-    .to(".screen--4 .anim-text", 1,
+    .to(".screen--3 .anim-img", 2, 
+      {scale: 2, y: "75%", ease: Linear.easeNone}, '-=2')
+    // .screen--4 stage 1
+    .set(".screen--4 .anim-img", {className: "+=stage-1"}, "+=1")
+    .set(".screen--4 .dot--0", {className: "-=active"})
+    .set(".screen--4 .dot--1", {className: "+=active"})
+    .to(".screen--4 .anim-text", 3, 
+      {y: "0%", ease: Linear.easeNone}, "+=2")
+    .to(".anim-bg.bl", 3, 
+      {opacity: 1, ease: Linear.ease}, '-=2')
+    .to(".screen--4 .anim-text", 3,
       {y: "-130%", ease: Linear.easeNone})
-    .to(".anim-bg.bl", 1, 
-      {opacity: 0, ease: Linear.ease}, '-=0.7')
-    .to(".screen--4", 1, 
+    .to(".anim-bg.bl", 3, 
+      {opacity: 0, ease: Linear.ease}, '-=2')
+    .set(".screen--4 .anim-img", {className: "+=stage-out"})
+    .set(".screen--4 .slide--0 .js-tp", {className: "+=active"})
+    .set(".screen--4 .slide--0 .js-tp", {className: "-=active"}, '+=2')
+    .to(".screen--4", 2, 
       {y: "-100%", ease: Linear.easeNone})
-    .to(".screen--4 .anim-img", 1, 
-      {scale: 1.1, ease: Linear.easeNone}, '-=1')
-    .to(".screen--5 .anim-img", 1, 
-      {scale: 2, x: "3%", y: "-55%", ease: Linear.easeNone})
-    .to(".screen--5 .slide--0 .anim-text", 1, 
-      {y: "0%", ease: Linear.easeNone})
-    .to(".anim-bg.br", 1, 
-      {opacity: 1, ease: Linear.ease}, '-=0.7')
-    .to(".screen--5 .slide--0 .anim-text", 1,
+    .to(".screen--4 .anim-img", 2, 
+      {scale: 1.1, ease: Linear.easeNone}, '-=2')
+    // .screen--5 stage 1
+    .set(".screen--5 .anim-img", {className: "+=stage-1"}, "+=1")
+    .set(".screen--5 .dot--0", {className: "-=active"})
+    .set(".screen--5 .dot--1", {className: "+=active"})
+    .to(".screen--5 .slide--0 .anim-text", 3, 
+      {y: "0%", ease: Linear.easeNone}, "+=2")
+    .to(".anim-bg.br", 3, 
+      {opacity: 1, ease: Linear.ease}, '-=2')
+    .to(".screen--5 .slide--0 .anim-text", 3,
       {y: "-130%", ease: Linear.easeNone})
-    .to(".anim-bg.br", 1, 
-      {opacity: 0, ease: Linear.ease}, '-=0.7')
-    .to(".screen--5 .anim-img", 1, 
-      {scale: 2.2, x: "-105%", y: "-15%", ease: Linear.easeNone})
-    .to(".screen--5 .slide--1 .anim-text", 1, 
-      {y: "0%", ease: Linear.easeNone})
-    .to(".anim-bg.ll", 1, 
-      {opacity: 1, ease: Linear.ease}, '-=0.7')
-    .to(".screen--5 .slide--1 .anim-text", 1,
+    .to(".anim-bg.br", 3, 
+      {opacity: 0, ease: Linear.ease}, '-=2')
+    .set(".screen--5 .slide--0 .js-tp", {className: "+=active"})
+    .set(".screen--5 .slide--0 .js-tp", {className: "-=active"}, '+=2')
+    // .screen--5 stage 2
+    .set(".screen--5 .anim-img", {className: "+=stage-2"}, "+=1")
+    .set(".screen--5 .dot--1", {className: "-=active"})
+    .set(".screen--5 .dot--2", {className: "+=active"})
+    .to(".screen--5 .slide--1 .anim-text", 3, 
+      {y: "0%", ease: Linear.easeNone}, "+=2")
+    .to(".anim-bg.ll", 3, 
+      {opacity: 1, ease: Linear.ease}, '-=2')
+    .to(".screen--5 .slide--1 .anim-text", 3,
       {y: "-130%", ease: Linear.easeNone})
-    .to(".anim-bg.ll", 1, 
-      {opacity: 0, ease: Linear.ease}, '-=0.7');
+    .to(".anim-bg.ll", 3, 
+      {opacity: 0, ease: Linear.ease}, '-=2')
+    .set(".screen--5 .slide--1 .js-tp", {className: "+=active"});
   
   new ScrollMagic.Scene({
     triggerElement: '.page',
     triggerHook: "onLeave",
-    duration: "500%"
+    duration: "800%"
   })
     .setPin(".page")
     .setTween(tl)
     .addTo(controller);
+  
+  $('.js-trigger').hover(function(){
+    $(this).siblings('.js-product').addClass('open');
+  });
 }
 
 module.exports = {init};
